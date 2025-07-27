62746b3d7badec27a89324820bfb94352a547079
kzhangdz <kevin.zhang@detroitmi.gov>
2025-07-27 12:50:23 -0400

    initial pdf file merge

---
+++ main.py
@@ -0,0 +1,195 @@
+import tkinter.font as tkFont
+import tkinter as tk
+from tkinter import ttk, filedialog
+from tkinterdnd2 import DND_FILES, TkinterDnD # used to allow drag and drop
+import sv_ttk # styling package
+
+from collections.abc import Callable
+from functools import partial # for callback functions in buttons, with arguments
+from pypdf import PdfWriter
+from typing import List, Dict
+
+#https://www.youtube.com/watch?v=VEdaDzI9rJM
+
+DEFAULT_PADDING = 20
+DEFAULT_BUTTON_WIDTH = 6
+DEFAULT_FONT = "Arial"
+title_font = tkFont.Font(font=DEFAULT_FONT, size=12)
+
+def main():
+    app = Application()
+    app.mainloop()
+
+class Application(TkinterDnD.Tk): # can inherit, so our application itself is a tkinter Tk instance
+    
+    def __init__(self):
+        super().__init__()
+
+        self.title("PDF File Merger")
+        sv_ttk.set_theme("dark")
+
+        self.columnconfigure(0, weight=1)
+        self.rowconfigure(0, weight=1)
+
+        self.main_frame = MainPage(self)
+        self.main_frame.pack(fill=tk.BOTH, expand=True)
+
+        self.geometry("900x500")
+    
+        # self.frame = InputForm(self)
+        # subsection of window, helps organize widgets and allows reusability
+        # self.frame.grid(row=0, column=0, sticky="nsew", padx=5, pady=5) # sticky, so our frame expands with the window
+
+class MainPage(ttk.Frame):
+
+    def __init__(self, parent):
+        super().__init__(parent)
+
+        self.list_frame = ttk.Frame(self)
+        self.file_names_listbox = FileNamesListbox(self.list_frame)
+        self.file_names_listbox.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+        self.button_section = ButtonSection(self.list_frame, on_add_click=self.browse_files)
+        self.button_section.pack(side=tk.RIGHT, padx=DEFAULT_PADDING)
+        self.list_frame.pack(fill=tk.BOTH, expand=True)
+
+        self.bottom_frame = ttk.Frame(self)
+        self.target_file_selection_box = TargetFileSelectionBox(self.bottom_frame)
+        self.target_file_selection_box.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+        self.merge_section = MergeSection(self.bottom_frame, lambda: self.merge_files(self.file_names_listbox.get_content(), self.target_file_selection_box.get_save_path()))
+        self.merge_section.pack(side=tk.RIGHT)
+        self.bottom_frame.pack(fill=tk.BOTH)
+
+        self.message = tk.StringVar()
+        self.message.set("")
+        self.message_section = MessageSection(self, message=self.message, height=100)
+        self.message_section.pack(side=tk.BOTTOM, fill=tk.BOTH)
+
+    def browse_files(self):
+        files = filedialog.askopenfilenames(
+            title="Select PDF files to merge",
+            defaultextension=".pdf",
+            filetypes=[("PDF files", "*.pdf")]
+        )
+        
+        if files:
+            for file in files:
+                self.file_names_listbox.file_names_listbox.insert(tk.END, file)
+                print("Adding", file)
+
+    def merge_files(self, files: List[str], save_path: str):
+        
+        print("Merging Files")
+        
+        merger = PdfWriter()
+
+        for file in files:
+            merger.append(file)
+            print("Appending", file)
+
+        try:
+            merger.write(save_path)
+            print("Written to", save_path)
+
+            # modify the display message
+            self.message.set(f"Successfully written to {save_path}")
+        except:
+            self.message.set("Error occurred when merging")
+
+        merger.close()
+
+class ButtonSection(ttk.Frame):
+
+    def __init__(self, parent, on_add_click: Callable[..., None]=None, on_remove_click: Callable[..., None]=None):
+        super().__init__(parent)
+
+        self.add_button = ttk.Button(self, text="Add", command=on_add_click)
+
+        self.delete_button = ttk.Button(self, text="Remove", command=on_remove_click)
+
+        buttons = [self.add_button, self.delete_button]
+        for button in buttons:
+            button.config(width=DEFAULT_BUTTON_WIDTH)
+            button.pack(pady=DEFAULT_PADDING/4)
+
+class MessageSection(ttk.Frame):
+
+    def __init__(self, parent, message, **kwargs):
+        super().__init__(parent, **kwargs)
+
+        self.message_section = tk.Message(self, textvariable=message, width=300, font=title_font)
+        self.message_section.pack(fill=tk.BOTH, pady=DEFAULT_PADDING/5)
+
+class MergeSection(ttk.Frame):
+
+    def __init__(self, parent, callback):
+        super().__init__(parent)
+
+        self.merge_button = ttk.Button(self, text="Merge", command=callback)
+        self.merge_button.config(width=DEFAULT_BUTTON_WIDTH)
+        self.merge_button.pack()
+
+class TargetFileSelectionBox(ttk.Frame):
+
+    def __init__(self, parent):
+        super().__init__(parent)
+
+        # entry + Browse Button
+        self.entry = ttk.Entry(self) # TODO: need to be able to type the entry text
+        self.browse_button = ttk.Button(self, text="Browse", command=self.browse_file)
+        self.browse_button.config(width=DEFAULT_BUTTON_WIDTH)
+
+        self.entry.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+        self.browse_button.pack(side=tk.RIGHT)
+
+    def browse_file(self):
+        file_path = filedialog.asksaveasfilename(
+            title="Select PDF file to write to",
+            defaultextension=".pdf",
+            filetypes=[("PDF files", "*.pdf")]
+            )
+        
+        if file_path:
+            # Place the file path in the widget
+            print("Selected save path:", file_path)
+            self.entry.delete(0, tk.END)
+            self.entry.insert(0, file_path)
+
+    def get_save_path(self):
+        return self.entry.get()
+
+class FileNamesListbox(ttk.Frame):
+
+    def __init__(self, parent):
+        super().__init__(parent)
+
+        # main listbox
+        self.file_names_listbox = tk.Listbox(self, selectmode=tk.MULTIPLE, font=title_font)
+        self.scrollbar = ttk.Scrollbar(self, orient=tk.VERTICAL)
+
+        # connect listbox and scrollbar
+        self.file_names_listbox.config(yscrollcommand=self.scrollbar.set)
+        self.scrollbar.config(command=self.file_names_listbox.yview)
+
+        self.scrollbar.pack(side=tk.RIGHT, fill=tk.Y) # fill vertically
+        self.file_names_listbox.pack(side=tk.LEFT, fill=tk.BOTH, expand=True) # fill horizontally and vertically
+
+        self.file_names_listbox.drop_target_register(DND_FILES)
+        self.file_names_listbox.dnd_bind('<<Drop>>', self.drop_inside_list_box)
+        #self.file_names_listbox.dnd_bind('<<ListboxSelect>>', self.browse_files)
+
+    def drop_inside_list_box(self, event):
+        # get a list of file names. Helps handle cases with space in the file name
+        files = self.tk.splitlist(event.data)
+
+        #TODO: add logic to only add PDFs
+        #TODO: logic to not allow duplicates?
+
+        for file in files:
+            self.file_names_listbox.insert(tk.END, file)
+            print("Adding", file)
+
+    def get_content(self):
+        return self.file_names_listbox.get(0, tk.END)
+
+if __name__ == "__main__":
+    main()
